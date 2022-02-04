f4de2661030a7918ade499ee9c31aca4d1fd953b
lakshyamahawar14 <83651022+lakshyamahawar14@users.noreply.github.com>
2022-02-04 14:54:31 +0530

    adding new files
    adding new files

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const path = require("path");
+const addUser = require("./database.js");
+
+const app = express();
+const PORT = 5000;
+const staticPath = path.join(__dirname, "../public");
+
+app.use(express.static(staticPath));
+
+app.use(express.json());
+
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+  res.sendFile(staticPath + "/index.html");
+});
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+app.post("/leaderboard", (req, res) => {
+  var _username = req.body.username;
+  var _score = req.body.score;
+  addUser(_username, _score);
+  sleep(2000).then(() => {
+      res.sendFile(staticPath + '/scores.txt');
+  })
+});
+
+app.listen(PORT, (req, res) => {
+  console.log(`Server running on http://localhost:${PORT}/`);
+});
